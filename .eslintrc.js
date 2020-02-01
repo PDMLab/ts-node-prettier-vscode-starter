7c278d609f7d48b19367816603a8519778d5f348
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2020-02-01 16:21:13 +0100

    chore: setup project

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  env: {
+    node: true,
+    jest: true
+  },
+  plugins: ['@typescript-eslint', 'prettier']
+}
